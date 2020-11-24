78dd40068faeee7629bac8c5aabe6ff9cbe923e6
Abdelaali Es salmi <abdlalisalmi@gmail.com>
2020-11-24 19:28:51 +0100

    messing sky and floor color error handle

--- handle_floor_sky_color.c
+++ handle_floor_sky_color.c
@@ -6,11 +6,39 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/16 19:37:36 by aes-salm          #+#    #+#             */
-/*   Updated: 2020/10/29 19:40:33 by aes-salm         ###   ########.fr       */
+/*   Updated: 2020/11/24 19:27:40 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	get_color_value(const char *str)
+{
+	int		result;
+	int		sign;
+    int     value_found;
+
+	result = 0;
+	sign = 0;
+    value_found = -1;
+	while (str[file.i] != '\n' && !((str[file.i] >= '0') &&
+				(str[file.i] <= '9')) && str[file.i] != '-')
+		file.i++;
+	sign = (str[file.i] == '-') ? 1 : 0;
+	while (str[file.i] == '-' || str[file.i] == '+')
+		file.i++;
+	
+	while ((str[file.i] >= '0') && (str[file.i] <= '9'))
+	{
+        value_found = 1;
+		result = result * 10;
+		result += (int)str[file.i] - '0';
+		file.i++;
+	}
+    if (value_found < 0)
+        return (-1);
+	return (sign == 1) ? (-result) : result;
+}
 
 int hexa_to_decimal(char hexVal[]) 
 {    
@@ -57,13 +85,12 @@ void    handle_floor_sky_color(char *text)
 	char f_c;
 
 	f_c = text[file.i];
-    r = my_atoi(text);
-    g = my_atoi(text);
-    b = my_atoi(text);
+    r = get_color_value(text);
+    g = get_color_value(text);
+    b = get_color_value(text);
     if (r < 0 || g < 0 || b < 0)
     {
-        write(1, "Error\nthe color code should be between 0 and 255 !!\n", 52);
-        
+        write(1, "Error\nthe color RGB code should be between 0 and 255 !!\n", 56);
         exit(EXIT_FAILURE);
     }
     printf("r: %d\n", r);
